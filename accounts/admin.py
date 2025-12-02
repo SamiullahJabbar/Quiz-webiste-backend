dcbaacbe9283ac6bd51e015331a077e751870a40
SamiullahJabbar <samiullahjabbar65@gmail.com>
2025-12-02 22:18:08 +0500

    update admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -22,10 +22,56 @@ def send_rejection_email(user):
     email.content_subtype = "html"
     email.send()
 
+# @admin.register(User)
+# class UserAdmin(admin.ModelAdmin):
+#     list_display = ('id','email','first_name','last_name','education','is_active','is_staff')
+#     actions = ['approve_users','reject_users']
+
+#     def approve_users(self, request, queryset):
+#         for user in queryset:
+#             user.is_active = True
+#             user.save()
+#             send_approval_email(user)
+#     approve_users.short_description = "Approve selected users"
+
+#     def reject_users(self, request, queryset):
+#         for user in queryset:
+#             send_rejection_email(user)
+#             user.delete()
+#     reject_users.short_description = "Reject selected users"
+
+
+
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+
 @admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    list_display = ('id','email','first_name','last_name','education','is_active','is_staff')
-    actions = ['approve_users','reject_users']
+class CustomUserAdmin(BaseUserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = User
+
+    list_display = ('id', 'email', 'first_name', 'last_name', 'education', 'is_active', 'is_staff', 'otp_verified')
+    ordering = ('email',)
+    search_fields = ('email', 'first_name', 'last_name')
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password', 'otp_code', 'otp_verified')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'education', 'date_of_birth')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login',)}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'first_name', 'last_name', 'education', 'date_of_birth', 'password1', 'password2'),
+        }),
+    )
+
+    actions = ['approve_users', 'reject_users']
 
     def approve_users(self, request, queryset):
         for user in queryset:
